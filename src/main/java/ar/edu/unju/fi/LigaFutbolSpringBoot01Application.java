1df9c04feef6de3edc916477696131a627b5e20c
aleajndro marcelo <alemar050812@gmail.com>
2020-05-16 17:21:04 -0300

    Asunto:Inicializacion de Proyecto Responsable:Alejjandro Tarea01:Inicializacion de Proyecto

---
+++ src/main/java/ar/edu/unju/fi/LigaFutbolSpringBoot01Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigaFutbolSpringBoot01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LigaFutbolSpringBoot01Application.class, args);
+	}
+
+}
